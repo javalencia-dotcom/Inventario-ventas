6d172a02c67a62c39ac04b2c5169d93b32431d6e
javalencia-dotcom <ja.valencia@eston.ecotec.edu.ec>
2026-05-01 16:17:16 -0500

    Update VentaService.php

--- Proyecto_integrador_final/services/VentaService.php
+++ Proyecto_integrador_final/services/VentaService.php
@@ -2,17 +2,29 @@
 
 class VentaService {
 
+    public function validarVenta($stock, $cantidad) {
+
+        if ($cantidad <= 0) {
+            return [
+                "estado" => false,
+                "mensaje" => "La cantidad debe ser mayor a 0"
+            ];
+        }
+
+        if ($cantidad > $stock) {
+            return [
+                "estado" => false,
+                "mensaje" => "Stock insuficiente para realizar la venta"
+            ];
+        }
+
+        return [
+            "estado" => true,
+            "mensaje" => "Venta válida"
+        ];
+    }
+
     public function calcularTotal($cantidad, $precio) {
         return $cantidad * $precio;
     }
-
-    public function validarStock($stock, $cantidad) {
-
-        if ($cantidad > $stock) {
-            return false;
-        }
-
-        return true;
-    }
-
 }
